bd3ca6ea948ffa5bc1928cd32e345e3a2a1e98d3
Krste <toshev.krste@yahoo.com>
2023-10-04 16:10:36 +0200

    Error handling: Place non-existent or spelling error

--- main.py
+++ main.py
@@ -9,26 +9,31 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-if place:
-    weather_data = get_weather_data(place, days, option)
-    if option == "Temperature":
-        temp_data = []
-        temp_date = []
-        for data in weather_data:
-            temp_data.append(data['temperature'])
-            temp_date.append(data['date'])
-        # Temperature plot
-        figure = px.line(x=temp_date, y=temp_data,
-                         labels={"x": "Date", "y": "Temperature (C)"})
-        st.plotly_chart(figure)
+try:
+    if place:
+        weather_data = get_weather_data(place, days, option)
+        if option == "Temperature":
+            temp_data = []
+            temp_date = []
+            for data in weather_data:
+                temp_data.append(data['temperature'])
+                temp_date.append(data['date'])
+            # Temperature plot
+            figure = px.line(x=temp_date, y=temp_data,
+                             labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-    if option == "Sky":
-        # Sky render
-        sky_data = []
-        sky_img = []
-        sky_date = []
-        for data in weather_data:
-            sky_data.append(data['description'])
-            sky_img.append(data['img'])
-            sky_date.append(data['date'])
-        st.image(sky_img, width=100, caption=sky_date)
+        if option == "Sky":
+            # Sky render
+            sky_data = []
+            sky_img = []
+            sky_date = []
+            for data in weather_data:
+                sky_data.append(data['description'])
+                sky_img.append(data['img'])
+                sky_date.append(data['date'])
+            st.image(sky_img, width=100, caption=sky_date)
+except KeyError:
+    place_error_message = """Unfortunately, we don't have weather information for the city you searched.
+    Please check the spelling or try a different city nearby"""
+    st.info(place_error_message)
